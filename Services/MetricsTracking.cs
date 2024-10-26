6906a205948021eae1eef69f373626d42ed37514
kate1234567 <47985697+kate1234567@users.noreply.github.com>
2024-10-27 01:22:21 +0300

    laboratory3

---
+++ Services/MetricsTracking.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using IO.Swagger.Services; 
+
+namespace IO.Swagger.Middlewares 
+{
+    public class MetricsTracking
+    {
+        private readonly RequestDelegate _next;
+        private readonly MetricsService _metricsService;
+
+        public MetricsTracking(RequestDelegate next, MetricsService metricsService)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+            _metricsService = metricsService ?? throw new ArgumentNullException(nameof(metricsService));
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var path = httpContext.Request.Path.Value;
+
+            if (path == "/metrics")
+            {
+                await _next.Invoke(httpContext);
+                return;
+            }
+
+            _metricsService.BeginConnection();
+            _metricsService.TrackRequest(httpContext.Request.Path, httpContext.Request.Method);
+
+            _metricsService.TrackCpuUsage();
+
+            var stopwatch = Stopwatch.StartNew();
+
+            try
+            {
+                await _next.Invoke(httpContext);
+            }
+            finally
+            {
+                stopwatch.Stop();
+
+                _metricsService.TrackResponse(httpContext.Response.StatusCode);
+
+                _metricsService.TrackResponseTime(stopwatch.Elapsed);
+
+                _metricsService.EndConnection();
+            }
+        }
+    }
+}
